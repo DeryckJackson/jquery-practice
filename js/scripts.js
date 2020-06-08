0ba5445f63c7982c2453d968ae12d4e0a5c1718f
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-06-08 14:02:21 -0700

    what the exercise is asking for doesn't make any sense. You wouldn't use a loop for event lisenters
    co-authored-by:  Ester Tataje <ester.tataje@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-  
+  let messages = ["This is a header.", "This is a paragraph.", "This is an image."]
+  $(this).click(function() {
+    if(this === "h1") {
+      alertMessage(messages[0]);
+    } else if (this === "p") {
+      alertMessage(messages[1]);
+    } else {
+      alertMessage(messages[2]);
+    }
+  });
+
 });
+
+const alertMessage = function (message) {
+  alert(message);
+}
